c67567fbd55651a7471b3748f7991a7f646bcab4
Davy Coelho <salomao_jesus1994@outlook.com>
2025-03-02 00:03:16 -0300

    Update Database.hs

--- src/Database.hs
+++ src/Database.hs
@@ -21,3 +21,36 @@ getRandomMonster config = do
   r <- query_ conn "SELECT * from monsters ORDER BY RANDOM() LIMIT 1" :: IO [M.Monster]
   close conn
   return $ head r 
+
+getCharacters :: Config -> IO [M.Character]
+getCharacters config = do
+  conn <- open (dbPath config)
+  r <- query_ conn "SELECT * from characters" :: IO [M.Character]
+  close conn
+  return r
+
+getRandomCharacter :: Config -> IO M.Character
+getRandomCharacter config = do
+  conn <- open (dbPath config)
+  r <- query_ conn "SELECT * from characters ORDER BY RANDOM() LIMIT 1" :: IO [M.Character]
+  close conn
+  return $ head r
+
+  searchCharacters :: Config -> String -> IO [M.Character]
+  searchCharacters config searchString = do
+    conn <- open (dbPath config)
+    r <- query conn "SELECT * from characters WHERE name LIKE ?" (Only $ "%" ++ searchString ++ "%") :: IO [M.Character]
+    close conn
+    return r
+
+  searchMonsters :: Config -> String -> IO [M.Monster]
+
+  searchMonsters config searchString = do
+    conn <- open (dbPath config)
+    r <- query conn "SELECT * from monsters WHERE monsterName LIKE ?" (Only $ "%" ++ searchString ++ "%") :: IO [M.Monster]
+    close conn
+    return r
+
+
+
+  
