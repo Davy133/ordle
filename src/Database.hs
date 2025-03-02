2941472b019c918c0f8cd271440fad3fcc7ee54f
Davy Coelho <salomao_jesus1994@outlook.com>
2025-03-01 23:46:52 -0300

    feat(DB): improve database
    Co-Authored-By: Caua-Kelvin <140714560+Caua-Kelvin@users.noreply.github.com>
    Co-Authored-By: CeciliaLucena <140714456+CeciliaLucena@users.noreply.github.com>
    Co-Authored-By: Marinho-212 <184698792+Marinho-212@users.noreply.github.com>

--- src/Database.hs
+++ src/Database.hs
@@ -3,10 +3,10 @@
 module Database ( getMonsters, getRandomMonster) where
 
 import qualified Models as M
--- import           Control.Applicative
 import           Database.SQLite.Simple
--- import           Database.SQLite.Simple.FromRow
 import           Config
+-- import           Control.Applicative
+-- import           Database.SQLite.Simple.FromRow
 
 getMonsters :: Config -> IO [M.Monster]
 getMonsters config = do
