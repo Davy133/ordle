699b4177207c5f472465eaf22abdad58ba5dca81
Davy Coelho <salomao_jesus1994@outlook.com>
2025-03-05 22:05:18 -0300

    dwadawdaw

--- app/Main.hs
+++ app/Main.hs
@@ -10,10 +10,12 @@ import ClassicController
     ( getTodayCharacter, selectCurrentCharacter )
 import QuoteController
 import EmojiController
+import MonsterController
 
-import Database (getCharacterById, getAllCharacters)
+
+import Database (getCharacterById, getAllCharacters, getAllMonsters, getMonsterById)
 import Config (loadConfig)
-import Models (Character(..))
+import Models (Character(..), monsterName, image, monsterId)
 import System.IO (hSetEncoding, stdout, utf8) 
 import Data.List (isInfixOf)
 import Data.List.Split (splitOn)
@@ -30,16 +32,18 @@ setup window = do
     classicButton <- button # set text "Classic Mode"
     quotesButton <- button # set text "Quotes Mode"
     emojisButton <- button # set text "Emojis Mode"
+    monstersButton <- button # set text "Monsters Mode"
     otherModeButton <- button # set text "Other Mode"
 
     on UI.click classicButton $ \_ -> classicPage window
     on UI.click quotesButton $ \_ -> quotesPage window
     on UI.click emojisButton $ \_ -> emojisPage window
+    on UI.click monstersButton $ \_ -> monsterPage window
     on UI.click otherModeButton $ \_ -> otherModePage window
 
     menuContainer <- UI.div # set style [("display", "flex"), ("justify-content", "center"), ("align-items", "center"), ("height", "100vh"), ("flex-direction", "column")]
 
-    getBody window #+ [element menuContainer #+ [element classicButton, element quotesButton, element emojisButton, element otherModeButton]]
+    getBody window #+ [element menuContainer #+ [element classicButton, element monstersButton, element quotesButton, element emojisButton, element otherModeButton]]
     return ()
 
 classicPage :: Window -> UI ()
@@ -196,6 +200,52 @@ emojisPage window = do
     container <- UI.div # set style [("display", "flex"), ("justify-content", "center"), ("align-items", "center"), ("height", "100vh"), ("flex-direction", "column")]
 
     getBody window #+ [element container #+ [element characterSelect, element guessButton, element hintButton, element infoTable]]
+    return ()
+
+monsterPage :: Window -> UI ()
+monsterPage window = do
+    liftIO selectCurrentMonster
+    todayMonster <- liftIO getTodayMonster
+    case todayMonster of
+        Just monster -> do
+            let imageUrl = "monsters_images/" ++ Models.image monster
+            img <- loadFile "image/png" imageUrl
+            monsterImage <- UI.img # set UI.src img # set style [("filter", "blur(30px)")]
+            void $ getBody window #+ [element monsterImage, string $ "Today's monster is: " ++ Models.monsterName monster]
+        Nothing -> void $ getBody window #+ [string "Today's monster is not available"]
+    void $ return window # set title "Monster Mode"
+
+    guessInput <- input # set (UI.attr "placeholder") "Guess the monster"
+    guessButton <- button # set text "Guess"
+    hintButton <- button # set text "Hint"
+
+    infoTable <- table #+ [UI.tr #+ [th #. "header" #+ [string "Monster"]], UI.body]
+
+    config <- liftIO $ loadConfig "config.dhall"
+    allMonsters <- liftIO $ getAllMonsters config
+    let monsterOptions = Prelude.map (\monster -> option # set text (Models.monsterName monster) # set value (show $ Models.monsterId monster)) allMonsters
+    monsterSelect <- UI.select #+ monsterOptions
+
+    on UI.click guessButton $ \_ -> do
+        guess <- get value monsterSelect
+        let guessId = read guess :: Int
+        config <- liftIO $ loadConfig "config.dhall"
+        monster <- liftIO $ getMonsterById config guessId
+        todayMonster <- liftIO getTodayMonster
+        case (monster, todayMonster) of
+            (Just mon, Just todayMon) -> do
+                let color = if Models.monsterName mon == Models.monsterName todayMon then "green" else "red"
+                void $ element infoTable #+ [UI.tr #+ [td # set style [("color", color)] #+ [string $ Models.monsterName mon]]]
+            (Nothing, _) -> do
+                void $ element infoTable #+ [UI.tr #+ [td #+ [string "Monster not found"]]]
+            (_, Nothing) -> do
+                void $ element infoTable #+ [UI.tr #+ [td #+ [string "Today's monster is not available"]]]
+        return ()
+
+
+    container <- UI.div # set style [("display", "flex"), ("justify-content", "center"), ("align-items", "center"), ("height", "100vh"), ("flex-direction", "column")]
+
+    getBody window #+ [element container #+ [element monsterSelect, element guessButton, element hintButton, element infoTable]]
     return ()
 
 otherModePage :: Window -> UI ()
